01924a13fb6d90d777718e9ac06b8031b54a5356
Gustavo Zucolotto <guguzucoloto@gmail.com>
2020-06-28 20:30:40 -0300

    programação dos botões

--- Unit1.pas
+++ Unit1.pas
@@ -5,7 +5,8 @@ interface
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ToolWin, Vcl.ComCtrls,
-  System.ImageList, Vcl.ImgList, Vcl.StdCtrls, Vcl.ExtCtrls;
+  System.ImageList, Vcl.ImgList, Vcl.StdCtrls, Vcl.ExtCtrls, Data.DB, Vcl.Grids,
+  Vcl.DBGrids;
 
 type
   TForm1 = class(TForm)
@@ -13,6 +14,26 @@ type
     Label1: TLabel;
     ImageList1: TImageList;
     ToolBar1: TToolBar;
+    ToolButton1: TToolButton;
+    ToolButton2: TToolButton;
+    ToolButton3: TToolButton;
+    ToolButton4: TToolButton;
+    ToolButton5: TToolButton;
+    ToolButton6: TToolButton;
+    ToolButton7: TToolButton;
+    ToolButton8: TToolButton;
+    ToolButton9: TToolButton;
+    ToolButton10: TToolButton;
+    DataSource1: TDataSource;
+    GroupBox1: TGroupBox;
+    PageControl1: TPageControl;
+    TabSheet1: TTabSheet;
+    TabSheet2: TTabSheet;
+    GroupBox2: TGroupBox;
+    GroupBox3: TGroupBox;
+    DBGrid1: TDBGrid;
+    StatusBar1: TStatusBar;
+    procedure DataSource1StateChange(Sender: TObject);
   private
     { Private declarations }
   public
@@ -25,5 +46,10 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.DataSource1StateChange(Sender: TObject);
+begin
+asdas
+end;
 
 end.
